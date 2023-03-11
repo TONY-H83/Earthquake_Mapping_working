2c16f5f5812643c5e4ac86d2118b46b688d89c38
TONY-H83 <82713513+TONY-H83@users.noreply.github.com>
2023-03-11 08:02:51 -0600

    Add files via upload

---
+++ Mapping_GeoJSON_Points/Static/JS/logic.js
@@ -0,0 +1,72 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+// Coordinates for each point to be used in the line.
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"14",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// Grabbing our GeoJSON data.
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+  // We turn each feature into a marker on the map.
+  onEachFeature: function(feature, layer) {
+    console.log(feature);
+    layer.bindPopup();
+  }
+}).addTo(map); 
+
+// var startMarker = L.marker([37.6152, -122.3899]).addTo(map);
+
+// startMarker.bindPopup("<b>Start</b>").openPopup();
+
+// var marker = L.marker([36.5298, -87.3595]).addTo(map);
+
+// marker.bindPopup("<b>Home</b>").openPopup();
+
+  // Get data from cities.js
+// let cityData = cities;
+
+
+
+// Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+//     console.log(city)
+//     L.circleMarker(city.location, {
+//         radius: city.population/200000,
+//         color: "orange",
+//     //fillColor: 'yellow',
+//     })
+//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//   .addTo(map);
+// });
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
+
+//adding comment to test branch
---
+++ Mapping_Single_Points/Static/JS/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoidG9ueWhlaW1wZWwiLCJhIjoiY2xleXo2anZhMDZmajQwcDF3aWw3NDR5cSJ9.9qlip05wqSCHGdg_MXI9VA";
